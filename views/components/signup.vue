4a1e5851f0bba3e3112b87af10ac3e189a9dcc59
etiotan <etiotan@gmail.com>
2017-06-15 08:22:31 -0400

    v-if login

---
+++ views/components/signup.vue
@@ -0,0 +1,25 @@
+<template>
+<div class='login'>
+  <h1>Login</h1>
+  <form method='post' action='/login'>
+    <input type='hidden' name='_csrf' :value='csrfToken' required/>
+    <input type='text' placeholder='User Name' name='username' required/>
+    <br />
+    <input type='text' placeholder='Password' name='password' required/>
+    <br />
+    <input type='submit' />
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {}
+    }
+}
+</script>
+
+<style lang='css'>
+
+</style>
--- views/index.vue
+++ views/index.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="index">
   <h1>Welcome to Covenant</h1>
-  <a href="/login">Login</a>
-  <a href="/signup">Sign Up</a>
+
+  <div v-if="!user">
+    <a href="/login">Login</a>
+    <a href="/signup">Sign Up</a>
+  </div>
+  <div v-else>
+    <a href="/logout">Log Out</a>
+  </div>
+
 </div>
 </template>
 
