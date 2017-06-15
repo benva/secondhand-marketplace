3e65de0ca4b086e8f5f75ff6b96c0b3a3f414a1d
etiotan <etiotan@gmail.com>
2017-06-15 08:13:06 -0400

    can log in

--- views/components/login.vue
+++ views/components/login.vue
@@ -1,7 +1,14 @@
 <template>
-<div class="login">
+<div class='login'>
   <h1>Login</h1>
-
+  <form method='post' action='/login'>
+    <input type='hidden' name='_csrf' :value='csrfToken' required/>
+    <input type='text' placeholder='User Name' name='username' required/>
+    <br />
+    <input type='text' placeholder='Password' name='password' required/>
+    <br />
+    <input type='submit' />
+  </form>
 </div>
 </template>
 
@@ -13,6 +20,6 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang='css'>
 
 </style>
